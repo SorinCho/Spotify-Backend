58a3d1dc7c0e9c8b4dcb76582f00dedd0050ccc2
SorinCho <sorincho@gmail.com>
2021-01-05 17:27:21 -0500

    access and refresh as cookies, no database

--- passport-setup.js
+++ passport-setup.js
@@ -1,24 +1,26 @@
 const passport = require('passport');
 const SpotifyStrategy = require('passport-spotify').Strategy;
 // const keys = require("./keys");
-// const { client_id, client_secret, redirect_uri } = require('./config');
-const User = require('./models/user-model');
-// import spotifyApi from './spotify-setup';
+// const User = require('./models/user-model');
+
 // serialize the user.id to save in the cookie session
 // so the browser will remember the user when login
 passport.serializeUser((user, done) => {
-  done(null, user.id);
+  // console.log(user);
+  done(null, user);
 });
 
 // deserialize the cookieUserId to user in the database
-passport.deserializeUser((id, done) => {
-  User.findById(id)
-    .then((user) => {
-      done(null, user);
-    })
-    .catch((e) => {
-      done(new Error('Failed to deserialize an user'));
-    });
+passport.deserializeUser((obj, done) => {
+  console.log(obj);
+  done(null, obj);
+  // User.findById(id)
+  //   .then((user) => {
+  //     done(null, user);
+  //   })
+  //   .catch((e) => {
+  //     done(new Error('Failed to deserialize an user'));
+  //   });
 });
 
 passport.use(
@@ -29,27 +31,36 @@ passport.use(
       callbackURL: process.env.REDIRECT_URI,
     },
     async (accessToken, refreshToken, expires_in, profile, done) => {
-      // find current user in UserModel
-      // console.log(accessToken);
-      const currentUser = await User.findOne({
-        spotifyId: profile.id,
+      process.nextTick(() => {
+        // To keep the example simple, the user's spotify profile is returned to
+        // represent the logged-in user. In a typical application, you would want
+        // to associate the spotify account with a user record in your database,
+        // and return that user instead.
+        const obj = { accessToken, refreshToken };
+        return done(null, obj);
       });
-      // create new user if the database doesn't have this user
-      // spotifyApi.setAccessToken(accessToken);
-      if (!currentUser) {
-        const newUser = await new User({
-          spotifyId: profile.id,
-          accessToken,
-          refreshToken,
-        }).save();
-        if (newUser) {
-          done(null, newUser);
-        }
-      }
-      currentUser.accessToken = accessToken;
-      currentUser.refreshToken = refreshToken;
-      await currentUser.save();
-      done(null, currentUser);
+      // // find current user in UserModel
+      // // console.log(accessToken);
+      // // console.log(profile);
+      // const currentUser = await User.findOne({
+      //   spotifyId: profile.id,
+      // });
+      // // create new user if the database doesn't have this user
+      // // spotifyApi.setAccessToken(accessToken);
+      // if (!currentUser) {
+      //   const newUser = await new User({
+      //     spotifyId: profile.id,
+      //     accessToken,
+      //     refreshToken,
+      //   }).save();
+      //   if (newUser) {
+      //     done(null, newUser);
+      //   }
+      // }
+      // currentUser.accessToken = accessToken;
+      // currentUser.refreshToken = refreshToken;
+      // await currentUser.save();
+      // done(null, currentUser);
     },
   ),
 );
